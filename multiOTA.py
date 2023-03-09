25cc9222555911977bee002e99cc7bc0421887f7
Jim Teunis <jim.teunis@gmail.com>
2023-03-09 22:17:15 +0100

    run black

--- multiOTA.py
+++ multiOTA.py
@@ -23,26 +23,32 @@ amebaparty = AmebaParty(callback=update_callback)
 server_thread = threading.Thread(name="server", target=amebaparty.listen_blocking)
 server_thread.start()
 
-def show_info(title, message, selection_callback):
 
+def show_info(title, message, selection_callback):
     # guarantee these commands happen in the same frame
     with dpg.mutex():
-
         viewport_width = dpg.get_viewport_client_width()
         viewport_height = dpg.get_viewport_client_height()
 
         with dpg.window(label=title, modal=True, no_close=True) as modal_id:
             dpg.add_text(message)
-            dpg.add_button(label="Ok", width=75, user_data=(modal_id, True), callback=selection_callback)
+            dpg.add_button(
+                label="Ok",
+                width=75,
+                user_data=(modal_id, True),
+                callback=selection_callback,
+            )
 
     # guarantee these commands happen in another frame
     dpg.split_frame()
     width = dpg.get_item_width(modal_id)
     height = dpg.get_item_height(modal_id)
-    dpg.set_item_pos(modal_id, [viewport_width // 2 - width // 2, viewport_height // 2 - height // 2])
+    dpg.set_item_pos(
+        modal_id, [viewport_width // 2 - width // 2, viewport_height // 2 - height // 2]
+    )
+
 
 def on_selection(sender, unused, user_data):
-
     if user_data[1]:
         print("User selected 'Ok'")
     else:
