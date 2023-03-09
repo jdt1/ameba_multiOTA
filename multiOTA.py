8117543e31dfb5da1288f68d266c954b11577884
Jim Teunis <jim.teunis@gmail.com>
2023-03-09 17:24:32 +0100

    add messagebox

--- multiOTA.py
+++ multiOTA.py
@@ -23,6 +23,34 @@ amebaparty = AmebaParty(callback=update_callback)
 server_thread = threading.Thread(name="server", target=amebaparty.listen_blocking)
 server_thread.start()
 
+def show_info(title, message, selection_callback):
+
+    # guarantee these commands happen in the same frame
+    with dpg.mutex():
+
+        viewport_width = dpg.get_viewport_client_width()
+        viewport_height = dpg.get_viewport_client_height()
+
+        with dpg.window(label=title, modal=True, no_close=True) as modal_id:
+            dpg.add_text(message)
+            dpg.add_button(label="Ok", width=75, user_data=(modal_id, True), callback=selection_callback)
+
+    # guarantee these commands happen in another frame
+    dpg.split_frame()
+    width = dpg.get_item_width(modal_id)
+    height = dpg.get_item_height(modal_id)
+    dpg.set_item_pos(modal_id, [viewport_width // 2 - width // 2, viewport_height // 2 - height // 2])
+
+def on_selection(sender, unused, user_data):
+
+    if user_data[1]:
+        print("User selected 'Ok'")
+    else:
+        print("User selected 'Cancel'")
+
+    # delete window
+    dpg.delete_item(user_data[0])
+
 
 def callback(sender, app_data):
     global filename
@@ -45,6 +73,7 @@ def update_all(sender, app_data):
     print("Update all was clicked.")
     print("Filename: ", filename)
     amebaparty.perform_ota(filename)
+    show_info("Done", "OTA finished", on_selection)
 
 
 with dpg.window(tag="Update"):
@@ -55,7 +84,7 @@ with dpg.window(tag="Update"):
     )
     dpg.add_button(label="Update all robots", callback=update_all)
     dpg.add_button(label="Exit", callback=lambda: dpg.stop_dearpygui())
-    dpg.add_progress_bar(tag="progress", width=500, height=20)
+
 
 with dpg.file_dialog(
     directory_selector=False,
